7310049a3b65256afaf4d3161f3147bdb71b98a5
tuanflute275 <tuanflute275@gmail.com>
2024-10-23 10:57:59 +0700

    base asp web client call api

---
+++ ShopAppClient/Controllers/CategoryController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShopAppClient.Models;
+
+namespace ShopAppClient.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        string apiUrl = "https://localhost:7237/api/category";
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<ApiResponse>(apiUrl); 
+                var categories = response.Data;
+                return View(categories); 
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync(apiUrl, category);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Xử lý lỗi nếu thêm không thành công
+                    ModelState.AddModelError("", "Failed to create category.");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            string apiUrl = $"https://localhost:7237/api/category/{id}"; // URL lấy category theo id
+
+            try
+            {
+                var category = await _httpClient.GetFromJsonAsync<Category>(apiUrl);
+                return View(category);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            string apiUrl = $"https://localhost:7237/api/category/{category.CategoryId}"; // URL sửa category
+
+            try
+            {
+                var response = await _httpClient.PutAsJsonAsync(apiUrl, category);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Xử lý lỗi nếu sửa không thành công
+                    ModelState.AddModelError("", "Failed to update category.");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string apiUrl = $"https://localhost:7237/api/category/{id}"; // URL xóa category
+
+            try
+            {
+                var response = await _httpClient.DeleteAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Xử lý lỗi nếu xóa không thành công
+                    Console.WriteLine("errr");
+                    return null;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ ShopAppClient/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopAppClient.Models
+{
+    public class ApiResponse
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public List<Category> Data { get; set; }
+    }
+}
